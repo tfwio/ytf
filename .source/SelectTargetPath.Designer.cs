ce1eaf166104cc9aaaf029c7a0b755ed7b0c56aa
tfwio <tfwroble@gmail.com>
2018-03-27 06:07:35 -0500

    Template dialog for adding paths

---
+++ .source/SelectTargetPath.Designer.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * User: xo
+ * Date: 3/26/2018
+ * Time: 7:16 PM
+ */
+namespace YouTubeDownloadUI
+{
+  partial class SelectTargetPath
+  {
+    /// <summary>
+    /// Designer variable used to keep track of non-visual components.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button1;
+    
+    /// <summary>
+    /// Disposes resources used by the form.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing) {
+        if (components != null) {
+          components.Dispose();
+        }
+      }
+      base.Dispose(disposing);
+    }
+    
+    /// <summary>
+    /// This method is required for Windows Forms designer support.
+    /// Do not change the method contents inside the source code editor. The Forms designer might
+    /// not be able to load this method if it was changed manually.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // textBox1
+      // 
+      this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.textBox1.Location = new System.Drawing.Point(13, 13);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(397, 29);
+      this.textBox1.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(416, 13);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(103, 29);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // SelectTargetPath
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(530, 235);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox1);
+      this.Name = "SelectTargetPath";
+      this.Text = "SelectTargetPath";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+  }
+}
---
+++ .source/SelectTargetPath.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * User: xo
+ * Date: 3/26/2018
+ * Time: 7:16 PM
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace YouTubeDownloadUI
+{
+  /// <summary>
+  /// Description of SelectTargetPath.
+  /// </summary>
+  public partial class SelectTargetPath : Form
+  {
+    public SelectTargetPath()
+    {
+      InitializeComponent();
+    }
+  }
+}
