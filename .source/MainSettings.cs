6b646f97b0b9c9595147891859368e5b1ced46b3
tfwio <tfwroble@gmail.com>
2018-03-27 06:07:14 -0500

    add currently unused settings for future configuration file/options

---
+++ .source/MainSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+namespace YouTubeDownloadUI
+{
+	class MainSettings
+	{
+		public string TargetOutputDirectory {
+			get;
+			set;
+		}
+
+		public string PathFFmpeg {
+			get;
+			set;
+		}
+
+		public string PathYoutubeDL {
+			get;
+			set;
+		}
+
+		public List<string> DownloadTargets {
+			get;
+			set;
+		}
+	}
+}
+
+
