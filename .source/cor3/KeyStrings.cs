ef1cb6a34146902c29c8037c511f554a4732678d
tfwio <tfwroble@gmail.com>
2018-03-28 08:20:33 -0500

    more refactory, move RTF-text to KeyStrings.cs

--- .source/cor3/KeyStrings.cs
+++ .source/cor3/KeyStrings.cs
@@ -6,7 +6,16 @@ namespace YouTubeDownloadUtil
 		public const string FFmpeg = @"C:\DEV\avcvt-utils\ffmpeg-20180217-dd8351b-win64-static\bin\";
 
 		public const string YoutubeDL = @"C:\Users\xo\Desktop\youtube-dl-win\bin\";
+    public const string ShowHelp = @"A helper for running youtube-dl!
 
+First Steps:
+
+1, Go get youtube-dl
+2. Place it somewhere.
+3. DragDrop youtube-dl.exe into this app to tell it where it is.
+4. Go get FFmpeg and do the same.
+5. Go get AtomicParsley and do the same.
+";
 	}
 }
 
