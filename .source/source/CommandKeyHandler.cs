fe35d64d7295707a383e4d44594ff85aa009f5a1
tfwio <tfwroble@gmail.com>
2018-03-30 05:21:49 -0500

    CommandKeyHandler: Add support for container.

--- .source/source/CommandKeyHandler.cs
+++ .source/source/CommandKeyHandler.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 namespace YouTubeDownloadUtil
 {
+  class CommandKeyHandler<TParent>
+  {
+    public string Name { get; set; }
+    public System.Windows.Forms.Keys Keys { get; set; }
+    public Action<TParent> Action { get; set; }
+  }
 	class CommandKeyHandler
 	{
 	  public string Name { get; set; }
@@ -8,7 +14,4 @@ namespace YouTubeDownloadUtil
 		public Action Action { get; set; }
 	}
 }
-
-
-
 
