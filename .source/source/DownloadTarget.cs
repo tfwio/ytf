908d9ec7f394722beb9289e4603d2f0eaba8dd64
tfwio <tfwroble@gmail.com>
2018-03-31 13:07:59 -0500

    I thought this was commited?!  from prior Flags as opposed to Properties transition...

--- .source/source/DownloadTarget.cs
+++ .source/source/DownloadTarget.cs
@@ -1,8 +1,9 @@
-﻿using System;
-namespace YouTubeDownloadUtil
+﻿namespace YouTubeDownloadUtil
 {
-	class DownloadTarget
+  class DownloadTarget
 	{
+	  public YoutubeDlFlags Flags { get; set; }
+	  
 		public string TargetUri { get; set; }
 
 		public string KnownTargetFile { get; set; }
@@ -10,19 +11,8 @@ namespace YouTubeDownloadUtil
 		public string TargetType { get; set; } // m4a, best, mp4, mp3, ogg
 		public string SubLang { get; set; }
 		
-    public bool AbortOnDuplicate { get; set; }
     public string AbortMessage { get; set; }
 		
-		public bool AddMetaData { get; set; }
-		public bool Continue { get; set; } // -c
-		public bool EmbedSubs { get; set; } // --embed-subs
-		public bool EmbedThumbnail { get; set; } 
-		public bool GetPlaylist { get; set; } // --get-playlist
-		public bool IgnoreErrors { get; set; } // -i
-		public bool Verbose { get; set; } // --verbose
-		public bool WriteAutoSub { get; set; }
-		public bool WriteSub { get; set; }
-
 		public bool HasPlaylist { get { return TargetUri.Contains("&list=") || TargetUri.Contains("?list="); } } // only youtube.
 		public bool HasTargetType { get { return !string.IsNullOrEmpty(TargetType); } }
 		
@@ -30,18 +20,9 @@ namespace YouTubeDownloadUtil
 		static public DownloadTarget GenerateDefault {
 			get {
 				return new DownloadTarget() {
+		      Flags = ConfigModel.Instance.AppFlags,
           TargetType = "best",
 					SubLang = "en",
-          AbortOnDuplicate = true,
-          AddMetaData = true,
-          Continue = true,
-          EmbedSubs = true,
-          EmbedThumbnail = true,
-          GetPlaylist = false,
-					IgnoreErrors = true,
-          Verbose = true,
-          WriteAutoSub = true,
-          WriteSub = true,
 				};
 			}
 		}
