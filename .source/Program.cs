e005f3204dba647227ab45b5c2c564ca114d3558
tfwio <tfwroble@gmail.com>
2018-09-04 17:35:48 -0500

    embedded config.ini

--- .source/Program.cs
+++ .source/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace YouTubeDownloadUtil
@@ -8,6 +9,15 @@ namespace YouTubeDownloadUtil
     [STAThread]
     private static void Main(string[] args)
     {
+      if (!File.Exists("config.ini")){
+        using (var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("YouTubeDownloadUtil.config.ini"))
+          using (var reader = new StreamReader(stream))
+        {
+          var content = reader.ReadToEnd();
+          File.WriteAllText(Path.Combine(System.DirectoryHelper.ExecutableDirectory, ResourceStrings.ConfDefault), content);
+        }
+      }
+      
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new MainForm());
