55607b18ac706b5e1deeb8a0f5fbc123358718a8
tfwio <tfwroble@gmail.com>
2018-09-05 04:54:32 -0500

    more (ms)build options

---
+++ .source/Build-AllCPU-Debug.cmd
@@ -0,0 +1,7 @@
+#! cmd.exe /c
+:: '#!' is not recognized as an internal or external command, operable program or batch file.
+@echo off
+set path1=%PROGRAMFILES(X86)%\msbuild\14.0\bin
+set path2=%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
+set PATH=%PATH%;%path1%
+msbuild %~dp0..\.solution\youtube-dl-winforms.sln /m /t:YouTubeDownloadUtil "/p:Configuration=Debug;Platform=Any CPU"
